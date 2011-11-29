f2f7e5bf9b7ef9ecd56056a9637572272bee9bc2
Alex Stapleton <alexs@prol.etari.at>
2011-11-29 12:35:33 +0000

    POST handler that lets you submit messages to pre-existing forums

--- raas/src/entrypoints/forums.cc
+++ raas/src/entrypoints/forums.cc
@@ -40,7 +40,7 @@ void ep_forum_index(evhttp_request* req) {
     evbuffer_free(resp);
 }
 
-void ep_forum_thread(evhttp_request* req) {
+void ep_forum_thread_GET(evhttp_request* req) {
     // extract the path from the URI (drop query and fragment) and decode it
     const struct evhttp_uri* uri = evhttp_request_get_evhttp_uri(req);
     std::string path(evhttp_uridecode(evhttp_uri_get_path(uri), 0, NULL));
@@ -67,9 +67,9 @@ void ep_forum_thread(evhttp_request* req) {
     if(down_cstr) traverse_depth = strtol(down_cstr, NULL, 10);
     if(errno != 0) traverse_depth = 1;
       
-    std::cout << "forum=" << forum_id << std::endl;
-    std::cout << "parent=" << root_parent_id << std::endl;
-    std::cout << "depth=" << traverse_depth << std::endl;
+    //std::cout << "forum=" << forum_id << std::endl;
+    //std::cout << "parent=" << root_parent_id << std::endl;
+    //std::cout << "depth=" << traverse_depth << std::endl;
 
     std::list<ThreadInfoSummary> root_threads;
 
@@ -89,7 +89,7 @@ void ep_forum_thread(evhttp_request* req) {
         for(;;++start) {
             ThreadInfoSummary& m = *start;
 
-            std::cout << depth << " " << m.msgId << " parent=" << m.parentId << std::endl;
+            //std::cout << depth << " " << m.msgId << " parent=" << m.parentId << std::endl;
 
             auto json_msg = json_object();
             json_object_set_new(json_msg, "id", json_string(m.msgId.c_str()));
@@ -118,4 +118,74 @@ void ep_forum_thread(evhttp_request* req) {
 
     return evhttp_send_json_reply(req, jroot);
 }
+
+
+void ep_forum_thread_POST(evhttp_request* req) {
+    // extract the path from the URI (drop query and fragment) and decode it
+    const struct evhttp_uri* uri = evhttp_request_get_evhttp_uri(req);
+    std::string path(evhttp_uridecode(evhttp_uri_get_path(uri), 0, NULL));
+
+    // pull the UID out on it's own because we need to special case
+    // it as part of the path stack
+    // FIXME: note the root URL structure is implied here. this is bad. we should
+    // be able to eliminate this with the help of url routers output
+    // but that requires a smarter request object
+    std::list<std::string> path_parts;
+    boost::split(path_parts, path, boost::is_any_of("/"));
+    // drop various bits off the front of the path
+    path_parts.pop_front(); // ("")
+    path_parts.pop_front(); // ("forum")
+    std::string forum_id = path_parts.front();
+
+    // fetch the post body into a vector<char>
+    evbuffer* postbody = evhttp_request_get_input_buffer(req);
+    size_t postlen = evbuffer_get_length(postbody);
+    std::vector<char> buf(postlen+1, '\0');
+    evbuffer_remove(postbody, &buf.front(), postlen);
+
+    // parse the query string and convert it to a wstring
+    struct evkeyvalq head;
+    evhttp_parse_query_str(&buf.front(), &head);
+    const char* body_cstr = evhttp_find_header(&head, "body");
+    const char* subject_cstr = evhttp_find_header(&head, "subject");
+    const char* parent_id_cstr = evhttp_find_header(&head, "parent");
+
+    // FIXME: return error about the message structure
+    assert(subject_cstr && body_cstr && parent_id_cstr);
+
+    // expand the utf8 out into wchars
+    std::wstring body_wstr(QString::fromUtf8(body_cstr).toStdWString());
+    std::wstring subject_wstr(QString::fromUtf8(subject_cstr).toStdWString());
+
+    // build the message object to send out
+    ForumMsgInfo new_message;
+    new_message.forumId = forum_id;
+    new_message.parentId = parent_id_cstr;
+    new_message.title = subject_wstr;
+    new_message.msg = body_wstr;
+
+    // push the message over to RS
+    if(rsForums->ForumMessageSend(new_message)) {
+        auto jroot = json_object();
+        json_object_set_new(jroot, "new_message_id", json_string(new_message.msgId.c_str()));
+        evhttp_send_json_reply(req, jroot);
+    }
+    else
+    {
+        // FIXME: use a sensible http return value
+        return ep_http_500(req);
+    }
+}
+
+void ep_forum_thread(evhttp_request* req) {
+    evhttp_cmd_type method = evhttp_request_get_command(req);
+    if(method == evhttp_cmd_type::EVHTTP_REQ_GET) {
+        ep_forum_thread_GET(req);
+    } else if(method == evhttp_cmd_type::EVHTTP_REQ_POST) {
+        ep_forum_thread_POST(req);
+    }
+}
+
+
+
 };
