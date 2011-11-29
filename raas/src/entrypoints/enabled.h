d6d025d110defb00f37ad6cd28331a33258b1b9a
Alex Stapleton <alexs@prol.etari.at>
2011-11-29 12:13:07 +0000

    added a trivial HTTP 500 error type

--- raas/src/entrypoints/enabled.h
+++ raas/src/entrypoints/enabled.h
@@ -8,6 +8,22 @@ void ep_file_share_browse(evhttp_request*);
 void ep_forum_index(evhttp_request*);
 void ep_forum_thread(evhttp_request*);
 
+
+
+
+static void ep_http_500(evhttp_request* req) {
+    struct evbuffer* resp = evbuffer_new();
+    evbuffer_add_printf(resp, "500 :(\n%s", evhttp_request_get_uri(req)); 
+
+    struct evkeyvalq* headers = evhttp_request_get_output_headers(req);
+    evhttp_add_header(headers, "Content-Type", "text/plain");
+    evhttp_send_reply(req, 500, "Internal Server Error", resp);
+    evbuffer_free(resp);
+}
+
+
+
+
 static void ep_http_404(evhttp_request* req) {
     struct evbuffer* resp = evbuffer_new();
     evbuffer_add_printf(resp, "404 :(\n%s", evhttp_request_get_uri(req)); 
