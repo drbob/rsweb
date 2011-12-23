a4090ecf54434a22fdff9d00f9880062063d4b1c
Alex Stapleton <alexs@prol.etari.at>
2011-12-23 12:11:18 +0000

    added OpenPGP-SDK and support in bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,11 +3,16 @@
 set -e
 set -x
 
+
 PREFIX=$(readlink -f build-env)
+OPENSSL_VERSION="0.9.8r"
+
+test -d $PREFIX || mkdir -v $PREFIX
 
 git submodule sync
 git submodule update
 
+# these are both fairly normal autotools projects
 cd boost.m4
 ./bootstrap
 ./configure --prefix="$PREFIX"
@@ -20,15 +25,27 @@ autoreconf -fvi
 make
 make install
 
+# purely JS stuff, just needs to exist in the right place so the rsjs 
+# symlink has something to point at
 cd ../
 hg clone https://code.google.com/p/pagedown/ || (cd pagedown && hg pull && hg update && cd ../)
 
-wget https://www.openssl.org/source/openssl-0.9.8r.tar.gz
-tar -zxvf openssl-0.9.8r.tar.gz
-cd ../openssl-0.9.8r
-./config --prefix="$PREFIX" threads shared static zlib-dynamic
+# not a normal autotools project, but does support make install, and --prefix etc.
+wget -O/dev/stdout https://www.openssl.org/source/openssl-$OPENSSL_VERSION.tar.gz | tar -zxv
+cd openssl-$OPENSSL_VERSION
+./config --prefix="$PREFIX" threads shared zlib-dynamic
 make
 make test
 make install
+
+# Uses it's own bizarro world build system that doesn't support make install
+# also needs to be made to point at the same openssl as the rest of the prohject
+# must point at the openssl source folder, not build-env because of how derped the build
+# scripts are
+cd ../OpenPGP-SDK
+./configure --without-idea --with-openssl=$(readlink -f ../openssl-$OPENSSL_VERSION)
+make
+cp -vR include/ "$PREFIX/"
+cp -vR lib/ "$PREFIX/"
 
 
